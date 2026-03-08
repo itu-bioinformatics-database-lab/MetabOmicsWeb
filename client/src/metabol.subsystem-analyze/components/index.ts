de4bc83bad52cd61b399aa6b044664445fcaf721
Anthony-Pius-1729 <tonypius1729@gmail.com>
2026-03-08 21:31:16 +0300

    feat: Introduce new miRNA, genomic variants, and epigenomics measurement components, refactor existing omics components, and update API dependencies and configuration.

--- client/src/metabol.subsystem-analyze/components/index.ts
+++ client/src/metabol.subsystem-analyze/components/index.ts
@@ -1,21 +1,22 @@
-export * from './subsystem-analyze.routes';
+export * from "./subsystem-analyze.routes";
 
-export * from './concentration-table'
-export * from './manual'
-export * from './upload';
-export * from './sample';
-export * from './Excel';
+export * from "./concentration-table";
+export * from "./manual";
+export * from "./upload";
+export * from "./sample";
+export * from "./Excel";
 
-export * from './subsystem-analyze.component';
-export * from './welcome/welcome.component';
+export * from "./subsystem-analyze.component";
+export * from "./welcome/welcome.component";
 
 // Omics components
-export * from './metabolomics/metabolomics.component';
-export * from './transcriptomics/transcriptomics.component';
-export * from './proteomics/proteomics.component';
-export * from './mirna/mirna.component';
+export * from "./metabolomics/metabolomics.component";
+export * from "./transcriptomics/transcriptomics.component";
+export * from "./proteomics/proteomics.component";
+export * from "./genomic-variants/genomic-variants.component";
+export * from "./mirna/mirnas.component";
 
 // Measurement components
-export * from './metabolomics-measurement/metabolomics-measurement.component';
+export * from "./metabolomics-measurement/metabolomics-measurement.component";
 
 // Upload components
--- client/src/metabol.subsystem-analyze/components/subsystem-analyze.routes.ts
+++ client/src/metabol.subsystem-analyze/components/subsystem-analyze.routes.ts
@@ -1,39 +1,68 @@
 import { SampleComponent } from "./sample/sample.component";
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
 import { AuthGuard } from "../../metabol.auth";
 
-import { ConcentrationTableComponent } from './concentration-table';
-import { ManualComponent } from './manual/manual.component';
-import { UploadComponent } from './upload/upload.component';
-import { SubsystemAnalyzeComponent } from './subsystem-analyze.component';
-import { ExcelComponent } from './Excel';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { MetabolomicsComponent } from './metabolomics/metabolomics.component';
-import { TranscriptomicsComponent } from './transcriptomics/transcriptomics.component';
-import { MetabolomicsMeasurementComponent } from './metabolomics-measurement/metabolomics-measurement.component';
-import { ProteomicsComponent } from './proteomics/proteomics.component';
-import { MirnaComponent } from './mirna/mirna.component';
+import { ConcentrationTableComponent } from "./concentration-table";
+import { ManualComponent } from "./manual/manual.component";
+import { UploadComponent } from "./upload/upload.component";
+import { SubsystemAnalyzeComponent } from "./subsystem-analyze.component";
+import { ExcelComponent } from "./Excel";
+import { WelcomeComponent } from "./welcome/welcome.component";
+import { MetabolomicsComponent } from "./metabolomics/metabolomics.component";
+import { TranscriptomicsComponent } from "./transcriptomics/transcriptomics.component";
+import { TranscriptomicsMeasurementComponent } from "./transcriptomics-measurement/transcriptomics-measurement.component";
+import { MetabolomicsMeasurementComponent } from "./metabolomics-measurement/metabolomics-measurement.component";
+import { ProteomicsComponent } from "./proteomics/proteomics.component";
+import { MirnaComponent } from "./mirna/mirnas.component";
 import { SubmitComponent } from "./submit/submit.component";
+import { GenomicVariantsComponent } from "./genomic-variants/genomic-variants.component";
+import { EpigenomicsComponent } from "./epigenomics/epigenomics.component";
+import { GenomicVariantsMeasurementComponent } from "./genomic-variants-measurement/genome-variants-measurement.component";
+import { EpigenomicsMeasurementComponent } from "./epigenomics-measurement/epigenomics-measurement.component";
+import { ProteomicsMeasurementComponent } from "./proteomics-measurement/proteomics-measurement.component";
+import { MirnaMeasurementComponent } from "./mirnas-measurement/mirnas-measurement.component";
 
 export const SubsystemAnalyzeRoutes: Routes = [
   {
-    path: 'analyze',
+    path: "analyze",
     component: SubsystemAnalyzeComponent,
     children: [
-      { path: 'welcome', component: WelcomeComponent },
-      { path: 'manual', component: ManualComponent },
-      { path: 'upload', component: UploadComponent },
-      { path: 'sample', component: SampleComponent },
-      { path: 'excel-data', component: ExcelComponent },
-      { path: 'metabolomics', component: MetabolomicsComponent },
-      { path: 'transcriptomics', component: TranscriptomicsComponent },
-      { path: 'metabolomics-measurement', component: MetabolomicsMeasurementComponent },
-      { path: 'proteomics', component: ProteomicsComponent },
-      { path: 'mirnas', component: MirnaComponent },
-      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-      { path: 'submit', component: SubmitComponent }
+      { path: "welcome", component: WelcomeComponent },
+      { path: "manual", component: ManualComponent },
+      { path: "upload", component: UploadComponent },
+      { path: "sample", component: SampleComponent },
+      { path: "excel-data", component: ExcelComponent },
+      { path: "metabolomics", component: MetabolomicsComponent },
+      { path: "transcriptomics", component: TranscriptomicsComponent },
+      {
+        path: "transcriptomics-measurement",
+        component: TranscriptomicsMeasurementComponent,
+      },
+      {
+        path: "metabolomics-measurement",
+        component: MetabolomicsMeasurementComponent,
+      },
+      { path: "genomic-variants", component: GenomicVariantsComponent },
+      {
+        path: "genomic-variants-measurement",
+        component: GenomicVariantsMeasurementComponent,
+      },
+      { path: "epigenomics", component: EpigenomicsComponent },
+      {
+        path: "epigenomics-measurement",
+        component: EpigenomicsMeasurementComponent,
+      },
+      {
+        path: "proteomics-measurement",
+        component: ProteomicsMeasurementComponent,
+      },
+      { path: "proteomics", component: ProteomicsComponent },
+      { path: "mirnas", component: MirnaComponent },
+      { path: "mirnas-measurement", component: MirnaMeasurementComponent },
+      { path: "", redirectTo: "welcome", pathMatch: "full" },
+      { path: "submit", component: SubmitComponent },
     ],
-    canActivate: [AuthGuard]
-  }
+    canActivate: [AuthGuard],
+  },
 ];
